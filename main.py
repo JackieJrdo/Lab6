945ffdf0455e0ca779a0819f37e8fe43bf86cd95
Jackie Jurado <jackiejrdo@gmail.com>
2023-10-25 10:20:14 -0400

    Improved encode function and incorporated it into main function, template set for decode function.

--- main.py
+++ main.py
@@ -10,11 +10,36 @@ def encode (password):
 
     return encodedStr
 
+def menu():
+    print("Menu\n -------------\n 1. Encode\n 2. Decode\n 3. Quit\n")
+    userOpt = int(input("Please enter an option: "))
+    userPass = input("Please enter your password to encode: ")
+
+
+
+
 
 if __name__ == '__main__':
+    userOpt = 1
 
-    password = '12345555'
-    p = encode(password)
-    print(p)
+    while userOpt != 3:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print("")
 
-    print("hello test 2")
+        userOpt = int(input("Please enter an option: "))
+
+        if userOpt == 1:
+            password = input("Please enter your password to encode: ")
+            ePass = encode(password)
+            print("Your password has been encoded and stored!")
+            print("")
+
+        #if userOpt == 2:
+            #decode(ePass)
+            #print("The encoded password is " + ePass + ", and the original password is " + password + ".")
+            #print("")
+

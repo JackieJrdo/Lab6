ee57b0332b19b2686a47e1e7fb6a9e271453cb9b
Jackie Jurado <jackiejrdo@gmail.com>
2023-10-25 09:55:18 -0400

    First Encode Test

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def encode (password):
+    encodedStr = ""
+
+    for i in password:
+        i = int(i)
+        i += 3
+        i = str(i)
+        encodedStr += i
+
+    return encodedStr
+
+
+if __name__ == '__main__':
+
+    password = '12345555'
+    p = encode(password)
+    print(p)
